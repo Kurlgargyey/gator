e578f8bb8bb4af2250ba8d4edd665b14a658c648
Kurlgargyey <nahsirkivar@gmail.com>
2025-02-07 13:25:26 +0100

    sql query to hopefully get all the posts from feeds a user is following

---
+++ sql/queries/posts.sql
@@ -0,0 +1,21 @@
+-- name: CreatePost :one
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES (
+    $1,
+    $2,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7
+)
+RETURNING *;
+
+-- name: GetPostsForUser :many
+WITH user_follows as (
+    SELECT feed_id from feed_follows
+    WHERE feed_follows.user_id = $1
+)
+SELECT posts.* FROM posts
+INNER JOIN user_follows ON posts.feed_id = user_follows.feed_id;
